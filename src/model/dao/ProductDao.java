3431bc390f6e194bb824e57d986ea94f801a8389
boreyreun168@gmail.com <boreyreun168@gmail.com>
2024-06-16 23:30:25 +0700

    update:first

--- src/model/dao/ProductDao.java
+++ src/model/dao/ProductDao.java
@@ -1,5 +1,13 @@
 package model.dao;
 
-public interface ProductDao {
+import model.entity.Product;
 
+import java.util.List;
+
+public interface ProductDao {
+    int addNewProduct(Product product);
+    int deleteProductById(Integer id);
+    int updateProductById(Integer id);
+    Product searchProductById(Integer id);
+    List<Product> queryAllProduct();
 }
